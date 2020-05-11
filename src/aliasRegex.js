9d4d8b7dce992ac9580cdbe6556480871395fe80
Felix C. Stegerman <flx@obfusk.net>
2020-05-11 17:52:10 +0200

    fix: firefox doesn't support lookbehind

--- src/aliasRegex.js
+++ src/aliasRegex.js
@@ -2,7 +2,7 @@ export const allowedAliasCharacters = "\\w\\-\\_\\+\\*\\(\\)\\!#&åô’çéãí
 export const startOfURL = "https?\\S*";
 
 function getAliasesRegex() {
-  return new RegExp(`(?<!${startOfURL}):([${allowedAliasCharacters}]+):`, "g");
+  return new RegExp(`(${startOfURL})?:([${allowedAliasCharacters}]+):`, "g");
 }
 
 export default getAliasesRegex;
