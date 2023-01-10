91eb24fffa27b02eefae1ced086dc69e5d9f706c
Gilles Hemmerlé <giloux@gmail.com>
2023-01-10 15:56:43 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ export function Twemoji({ svg, options, ...rest }) {
 
   options = {
     protocol,
-    baseUrl: `//cdnjs.cloudflare.com/ajax/libs/twemoji/12.0.4/${svg ? "svg/" : ""}`,
+    baseUrl: `//cdnjs.cloudflare.com/ajax/libs/twemoji/12.0.4/2/${svg ? "svg/" : ""}`,
     size,
     ext,
     ...options,
